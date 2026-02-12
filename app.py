71a7ba86436e5dccbee6c9708e46565cc90b8e03
evecus <jamie55@163.com>
2026-02-12 14:18:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cloudflared Pro Manager</title>
+    <title>Cloudflared Manager</title>
     <link rel="icon" href="https://www.cloudflare.com/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap" rel="stylesheet">
